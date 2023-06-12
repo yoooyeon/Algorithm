c7dbdb8b1e55ee0ca1a62cad9361d270ab3bb901
YEONI <89332391+yoooyeon@users.noreply.github.com>
2023-06-12 21:47:20 +0900

    Create 자동완성_fail.py
    모든 문자열의 prefix를 맵에 저장해 카운트하려고 했다. 
    TC 14, 19, 21, 22가 실패했다.
    문자열 갯수가 많아지고 길어질수록 효율이 떨어진다. O(N* M^2)
    N = 문자열 갯수, M = 문자 길이
    
    부분 문자열간의 연관관계를 생각한 자료구조로 바꿔야한다.

---
+++ Tree, Graph/자동완성_fail.py
@@ -0,0 +1,24 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/17685
+from collections import defaultdict
+
+
+def solution(words):
+    answer = 0
+    prefix_counter_dict = defaultdict(int)
+    for word in words:
+        for i in range(len(word)):
+            letter = word[:i + 1]
+            if letter in prefix_counter_dict.keys():
+                prefix_counter_dict[letter] += 1
+            else:
+                prefix_counter_dict[letter] = 1
+    for word in words:
+        for i in range(len(word)):
+            letter = word[:i + 1]
+            if prefix_counter_dict[letter] > 1:
+                answer += 1
+            else:
+                answer += 1
+                break
+
+    return answer
